da6f3a644e7d0890eaeb061860e2be0407bfc380
Erkmann <36168066+Erkmann@users.noreply.github.com>
2019-03-07 14:43:15 -0300

    Add files via upload

---
+++ copao/copaoifc/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CopaoifcConfig(AppConfig):
+    name = 'copaoifc'
---
+++ copao/copaoifc/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {'name': 'Index'}
+    return render(request, 'copaoifc/index.html',context)
+
+def login(request):
+    context = {'name' : 'Login'}
+    return render(request, 'copaoifc/index.html', context)
