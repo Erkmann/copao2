f1b64576c1a622b9e248f1a396395fbb5c910fb3
joao_vitor_santos <joaovitorjec@gmail.com>
2019-03-09 19:16:29 -0300

    A lot of things

--- copao/copaoifc/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    context = {'name': 'Index'}
-    return render(request, 'copaoifc/index.html',context)
-
-def login(request):
-    context = {'name' : 'Login'}
-    return render(request, 'copaoifc/index.html', context)
