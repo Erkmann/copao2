b3c506d10374a06585b68a46973209932043c48c
Russo <erkmann08@gmail.com>
2019-03-19 15:43:13 -0300

    BD terminado, admin pronto

--- apptimes/models.py
+++ apptimes/models.py
@@ -1,4 +1,7 @@
+from datetime import date
+from django import forms
 from django.db import models
+
 
 class Time(models.Model):
     logo = models.CharField(max_length = 55)
@@ -10,3 +13,41 @@ class Time(models.Model):
 
     def __str__(self):
         return self.nome_time
+
+class Partida(models.Model):
+    id_time_mandante = models.ForeignKey(Time, on_delete=models.DO_NOTHING, related_name='mandante')
+    id_time_visitante = models.ForeignKey(Time, on_delete=models.DO_NOTHING, related_name='visitante')
+    data = models.DateField(default=date.today)
+    gols_timeA = models.IntegerField(default=0)
+    gols_timeB = models.IntegerField(default=0)
+
+    def __str__(self):
+        nome = str(self.id_time_mandante) + ' X ' + str(self.id_time_visitante)
+        return nome
+
+class Jogador(models.Model):
+    numero_camisa = models.IntegerField(default=00)
+    nome = models.CharField(max_length=45)
+    gols = models.IntegerField(default=0)
+    cartao_amarelo = models.IntegerField(default=0)
+    cartao_vermelho = models.IntegerField(default=0)
+    id_time = models.ForeignKey(Time, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
+
+
+class Usuario(models.Model):
+    nome_usuario = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    senha = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.nome_usuario
+
+class Curtir(models.Model):
+    usuario_id_usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    time_id_time = models.ForeignKey(Time, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return str(self.usuario_id_usuario) + ' - ' + str(self.time_id_time)
--- copaoifc/admin.py
+++ copaoifc/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from apptimes.models import *
 # Register your models here.
+admin.site.register(Time)
+admin.site.register(Jogador)
+admin.site.register(Curtir)
+admin.site.register(Partida)
+admin.site.register(Usuario)
+
