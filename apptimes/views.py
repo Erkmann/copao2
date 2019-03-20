da27536039fdab589414cf299a3d9330382ad4a2
Erkmann <erkmann08@gmail.com>
2019-03-20 10:37:26 -0300

    template Time att

--- apptimes/views.py
+++ apptimes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from . models import Time
+from . models import Time, Jogador, Partida
+from django.db.models import Q
 
 def index(request):
    times =  Time.objects.all()
@@ -8,5 +9,7 @@ def index(request):
 
 def time(request, pk):
    time = Time.objects.get(id=pk)
-   context = {'time': time}
+   jogadores = Jogador.objects.filter(id_time=pk)
+   partidas = Partida.objects.filter(Q(id_time_mandante=pk) | Q(id_time_visitante=pk))
+   context = {'time': time, 'jogadores': jogadores, 'partidas': partidas}
    return render(request, 'apptimes/time.html', context)
